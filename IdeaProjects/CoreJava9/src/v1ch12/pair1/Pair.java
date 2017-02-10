128b8b39b4977a81156b1afe56b73ce6805f5cac
mrhuangyuhui <mrhuangyuhui@gmail.com>
2017-02-10 19:39:45 +0800

    +练习例子Java核心技术

---
+++ IdeaProjects/CoreJava9/src/v1ch12/pair1/Pair.java
@@ -0,0 +1,35 @@
+package v1ch12.pair1;
+
+/**
+ * Created by yuhuihuang on 10/02/2017.
+ */
+public class Pair<T> {
+
+    private T first;
+    private T second;
+
+    public Pair() {
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+}
